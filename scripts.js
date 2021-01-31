51d54f38185e18eab6992237b9008d13fa2d43a1
Nikolay Antonovich <tesalonick@gmail.com>
2021-01-31 15:34:43 +0300

    Task 1

---
+++ scripts.js
@@ -0,0 +1,8 @@
+console.log('Task 1');
+let name = prompt('Введитете ваше имя');
+let age = prompt('Введитете ваш возраст');
+let city = prompt('Введитете ваш город');
+let phone = prompt('Введитете ваш номер телефона');
+let email = prompt('Введитете ваш адрес электронной почты');
+let company = prompt('Введитете ваше место работы');
+console.log('Меня зовут', name+'. Мне', age, 'лет. Я проживаю в городе', city, 'и работаю в компании', company+'. Мои контактные данные:', phone+',', email+'.');
